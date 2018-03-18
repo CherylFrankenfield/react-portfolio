d5969fd27e51727b724c13134bd18a1fe02679c2
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-17 17:14:51 -0700

    Add header and footer styles and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
+const footer = {
+  display: 'flex',
+  fontSize: '20px',
+  justifyContent: 'space-around',
+  backgroundColor: 'grey',
+  width: '100%',
+  height: '150px',
+  color: 'white',
+  fontFamily: 'abril_fatfaceregular'
+};
+const ul = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+};
+
+const li = {
+  padding: '20px',
+  marginRight: '20px'
+};
+
+
 function Footer() {
   return(
-    <div>
-      Footer works!
+    <div style={footer}>
+      <div>
+        <h4>Contact Me:</h4>
+        <ul style={ul}>
+          <li style={li}>Email: cherylfrankenfield@gmail.com</li>
+          <li style={li}>Phone: 555.555.5555</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Find me online:</h4>
+        <ul style={ul}>
+          <li style={li}>LinkedIn</li>
+          <li style={li}>Instagram</li>
+          <li style={li}>Twitter</li>
+          <li style={li}>GitHub</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+const navContainer = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  backgroundColor: 'navy',
+  color: 'white',
+};
+const ul = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+};
+
+const li = {
+  alignSelf: 'center',
+  marginRight: '40px',
+  padding: '20px',
+  textTransform: 'uppercase',
+  fontSize: '16px'
+};
+
 function Nav() {
   return(
-    <div>
-      Nav works! 
+    <div style={navContainer}>
+      <ul style={ul}>
+        <li style={li}>About</li>
+        <li style={li}>Work</li>
+        <li style={li}>Experience</li>
+        <li style={li}>Contact</li>
+      </ul>
     </div>
   );
 }
