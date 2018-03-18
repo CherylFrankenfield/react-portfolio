1998ed683e8e089d82968d5e90f786fdc9623d36
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-17 19:04:30 -0700

    Style Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
+const hero = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  width: '100%',
+  height: '600px',
+  borderBottom: '2px solid white'
+};
+
+const paragraph = {
+  fontFamily: 'abril_fatfaceregular',
+  fontSize: '2em',
+  marginLeft: '50px',
+  width: '70%',
+  letterSpacing: '2px',
+  lineHeight: '40px'
+};
+
+const header = {
+  fontFamily: 'abril_fatfaceregular',
+  fontSize: '5em',
+  width: '100%',
+  marginLeft: '50px'
+};
+
 function Hero() {
   return(
-    <div>
-      Hero works
+    <div style={hero}>
+      <div>
+        <h1 style={header}>Hello. Nice to meet you.</h1>
+        <p style={paragraph}>I'm Cheryl Frankenfield and I'm a web developer based in the Pacific Northwest. I welcome you to take a look at my work and drop me a message.</p>
+      </div>
     </div>
   );
 }
 
 export default Hero;
+
+
+// background: url("../img/computer-bw.jpeg") center/cover no-repeat,
