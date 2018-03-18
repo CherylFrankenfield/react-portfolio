e1d63a8d79d4d9ed7e3ae4bc407ee50ea62d9bdf
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-17 19:19:17 -0700

    Add styles to Gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,81 @@
 import React from 'react';
 
+const gallery = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  backgroundColor: 'white',
+  borderBottom: '2px solid white',
+};
+
+const img = {
+  width: '100%',
+  minWidth: '310px',
+  height: '350px',
+  display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+  flex: '1 18%'
+};
+
+const text = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '1.4em',
+  width: '100%',
+  height: '25%',
+  backgroundColor: 'blue',
+  fontFamily: 'sans-serif'
+};
+
+const header = {
+  alignSelf: 'center',
+  margin: '5px',
+  padding: '2px'
+};
+
+const icon = {
+  color: 'black',
+  fontFamily: 'sans-serif',
+  overflow: 'hidden'
+};
+
 function Gallery() {
   return(
-    <div>
-      Gallery works
+    <div style={gallery}>
+      <div style={img}>
+        <div style={text}>
+          <h3 style={header}>Project: Beep Boop Machine</h3>
+        </div>
+        <div style={icon}>
+          <p></p>
+        </div>
+      </div>
+      <div style={img}>
+        <div style={text}>
+          <h3>Project: Pizza Ordering Site</h3>
+        </div>
+        <div style={icon}>
+          <p></p>
+        </div>
+      </div>
+      <div style={img}>
+        <div style={text}>
+          <h3>Project: Product Page</h3>
+        </div>
+        <div style={icon}>
+          <p></p>
+        </div>
+      </div>
+      <div style={img}>
+        <div style={text}>
+          <h3>Group Project: Cards Against Humanity</h3>
+        </div>
+        <div style={icon}>
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 }
