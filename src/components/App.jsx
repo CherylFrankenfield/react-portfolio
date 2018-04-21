6a2a31ea4eacf2e8e4a6fd8e9e689ea3c0b59ad5
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-04-21 12:42:11 -0700

    Add List and try to add icons to footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,21 @@ import Error404 from './Error404';
 import Home from './Home';
 import Nav from './Nav';
 import Footer from './Footer';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 function App() {
   return(
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route component={Error404} />
-      </Switch>
-      <Footer />
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route component={Error404} />
+        </Switch>
+        <Footer />
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,48 +1,68 @@
 import React from 'react';
-
-const footer = {
-  display: 'flex',
-  fontSize: '20px',
-  justifyContent: 'space-around',
-  backgroundColor: '#1A3A3A',
-  width: '100%',
-  height: '150px',
-  color: 'white',
-  fontFamily: 'abril_fatfaceregular'
-};
-const ul = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  listStyle: 'none',
-};
-
-const li = {
-  padding: '20px',
-  marginRight: '20px'
-};
-
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
 
 function Footer() {
   return(
-    <div style={footer}>
+    <div style={styles.footer}>
       <div>
         <h4>Contact Me:</h4>
-        <ul style={ul}>
-          <li style={li}>cherylfrankenfield@gmail.com</li>
-          <li style={li}>555.555.5555</li>
+        <ul style={styles.ul}>
+          <li style={styles.li}>cherylfrankenfield@gmail.com</li>
+          <li style={styles.li}>555.555.5555</li>
         </ul>
       </div>
       <div>
         <h4>Find me online:</h4>
-        <ul style={ul}>
-          <li style={li}><a href='https://www.linkedin.com/in/cherylfrankenfield/'>LinkedIn</a></li>
-          <li style={li}><a href='https://www.instagram.com/citysquirl/'>Instagram</a></li>
-          <li style={li}><a href='https://twitter.com/CherylFranken'>Twitter</a></li>
-          <li style={li}><a href='https://github.com/CherylFrankenfield'>GitHub</a></li>
-        </ul>
+        <List style={styles.ul}>
+          <ListItem style={styles.li} primaryText='LinkedIn' />
+          <ListItem style={styles.li} primaryText='Instagram'  />
+          <ListItem style={styles.li} primaryText='Twitter'  />
+          <ListItem style={styles.li} primaryText='GitHub' leftIcon={<ContentInbox />} />
+        </List>
       </div>
     </div>
   );
 }
 
+const styles = {
+  footer: {
+    display: 'flex',
+    fontSize: 20,
+    justifyContent: 'space-around',
+    backgroundColor: '#1A3A3A',
+    width: '100%',
+    height: 150,
+    color: 'white',
+    fontFamily: 'abril_fatfaceregular'
+  },
+  ul: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    listStyle: 'none',
+  },
+  li: {
+    fontSize: 20,
+    color: 'white',
+  }
+};
+
+
+
 export default Footer;
+
+// <ul style={styles.ul}>
+//   <li style={styles.li}><a href='https://www.linkedin.com/in/cherylfrankenfield/'>LinkedIn</a></li>
+//   <li style={styles.li}><a href='https://www.instagram.com/citysquirl/'>Instagram</a></li>
+//   <li style={styles.li}><a href='https://twitter.com/CherylFranken'>Twitter</a></li>
+//   <li style={styles.li}><a href='https://github.com/CherylFrankenfield'>GitHub</a></li>
+//   <li>
+//     <IconButton
+//       iconClassName='muidocs-icon-custom-github'
+//       tooltip='GitHub'
+//       iconStyle={styles.mediumIcon}
+//       style={styles.medium}
+//       href='https://github.com/CherylFrankenfield'
+//     />
+//   </li>
+// </ul>
